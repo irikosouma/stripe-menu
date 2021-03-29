bcae3c6ad4a92e05b7c718fec31d6409ed960071
ThanhIri <thanhnguyen@wee.vn>
2021-03-29 08:43:39 +0700

    up

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,12 +1,109 @@
 import React from 'react'
 //images
-import backGround from '../assets/images/background'
+import backGround from '../assets/images/background/background.svg'
+import Logo from '../assets/images/Logo'
+import Phone from '../assets/images/phone'
+import IconMenu from '../assets/images/iconMenu'
 //styles
 import {WrapMain} from './index.styles'
 export default function Main() {
+    const getXYofDiv = () => {
+        let elem = document.querySelector('section');
+        let rect = elem.getBoundingClientRect();
+        for (var key in rect) {
+        if(typeof rect[key] !== 'function') {
+            let para = document.createElement('p');
+            para.textContent  = `${ key } : ${ rect[key] }`;
+            document.body.appendChild(para);
+        }
+    }
+    // console.log('element rect', rect)
+    }
     return (
         <WrapMain backGround={backGround}>
-            mainn
+            <div className="wrap-content">
+                <div className="header-menu">
+                    <Logo />
+                    <div className="stripe-menu"   >
+                        <button className="btn-menu" onClick={getXYofDiv}>Products
+                            <span className="hover-content">
+                                Products
+                                <div className="wrap-icon">
+                                    <div>
+                                        <IconMenu />
+                                        Payment
+                                    </div>
+                                    <div>
+                                        <IconMenu />
+                                        Terminal
+                                    </div>
+                                    <div>
+                                        <IconMenu />
+                                        Connect
+                                    </div>
+                                </div>
+                            </span>
+                        </button>
+                        <button className="btn-menu" onClick={getXYofDiv}>Developers
+                            <span className="hover-content">
+                                Developers
+                                <div className="wrap-icon">
+                                    <div>
+                                        <IconMenu />
+                                        Plugin
+                                    </div>
+                                    <div>
+                                        <IconMenu />
+                                        Library
+                                    </div>
+                                    <div>
+                                        <IconMenu />
+                                        Help
+                                    </div>
+                                    <div>
+                                        <IconMenu />
+                                        Billing
+                                    </div>
+                                </div>
+                            </span>
+                        </button>
+                        <button className="btn-menu" onClick={getXYofDiv}>Company
+                            <span className="hover-content">
+                                Company
+                                <div className="wrap-icon">
+                                    <div>
+                                        <IconMenu />
+                                        About
+                                    </div>
+                                    <div>
+                                        <IconMenu />
+                                        Customer
+                                    </div>
+                                </div>
+                            </span></button>
+                    </div>
+                    <button className="btn-style">
+                        Sign in 
+                    </button>
+                </div>
+                <div className="wrap-main-content">
+                    <div className="main-title">
+                        <div className="title">
+                            Payments infrastructure for the internet
+                        </div>
+                        
+                        <div className="detail">
+                            Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.
+                        </div>
+                        <button className="btn-style">
+                            Start now
+                        </button>
+                    </div>
+                    <Phone className="image-style" />
+                </div>
+            </div>
+            {/* <section className="divv" onClick={getXYofDiv}></section> */}
+            
         </WrapMain>
     )
 }
--- src/Components/index.styles.js
+++ src/Components/index.styles.js
@@ -2,7 +2,113 @@ import styled from 'styled-components';
 const WrapMain = styled.div`
     width: 100%;
     height: 100%;
-    background-color: antiquewhite;
-    color: yellow;
+    background: url(${(props) => props.backGround});
+    background-repeat: no-repeat;
+    object-fit: cover;
+    background-size: contain;
+    .divv {
+        width: 400px;
+        height: 200px;
+        padding: 20px;
+        margin: 50px auto;
+        background: purple;
+    }
+    .wrap-content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        
+        .header-menu {
+            margin-top: 18px;
+            color: #fff;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            width: 47%;
+            .stripe-menu {
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                position: relative;
+                .hover-content {
+                    position: absolute;
+                    top: 40px;
+                    left: -230px;
+                    width: 560px;
+                    height: 80px;
+                    color: #102a42;
+                    background-color: #fff;
+                    box-shadow: 0 5px 15px rgb(0 0 0 / 20%);
+                    visibility: hidden;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: flex-start;
+                    justify-content: space-between;
+                    padding: 20px 30px;
+                    .wrap-icon {
+                        width: 100%;
+                        display: flex;
+                        flex-direction: row;
+                        align-items: center;
+                        justify-content: space-between;
+                    }
+                }
+
+                .btn-menu {
+                    cursor: pointer;
+                    color: #fff;
+                    width: 180px;
+                    height: 45px;
+                    background-color: transparent;
+                    appearance: none;
+                    border: none;
+                    font-size: 18px;
+                    :hover .hover-content {
+                        visibility: visible;
+                    }
+                }
+            }
+        }
+        .btn-style {
+            background-color: #222222;
+            width: fit-content;
+            padding: 5px 10px;
+            height: 30px;
+            color: #fff;
+            border-radius: 4px;
+            border: 2px solid #222222;
+            font-size: 14px;
+            :hover {
+                background-color: #617D99;
+            }
+        }
+        .wrap-main-content {
+            
+            width: 47%;
+            display: grid;
+            grid-template-columns: 1fr 330px;
+            grid-column-gap: 180px;
+            .main-title {
+                
+                padding-top: 39%;
+                .title {
+                    font-size: 88px;
+                    font-weight: 700;
+                    line-height: 88px;
+                    color: #102a42;
+                }
+                .detail {
+                    color: #617d98;
+                    font-size: 20px;
+                    line-height: 36px;
+                    padding: 20px 0 15px 0;
+                }
+            }
+            .image-style {
+                padding-top: 54%;
+            }
+        }
+    }
 `;
 export {WrapMain}
